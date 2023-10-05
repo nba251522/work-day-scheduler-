bec271112ee098d24d0c12918c677dbb351b9066
nba251522 <inquister112345678@gmail.com>
2023-10-05 15:23:23 -0700

    each timeblock will now pull any saved data from local storage to display on the webpage

--- Develop/script.js
+++ Develop/script.js
@@ -34,6 +34,12 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  $(".time-block").each(function() {
+    let blockHour = $(this).attr('id');
+    let storedValue = localStorage.getItem(blockHour);
+    if (storedValue) {
+        $(this).find('textarea').val(storedValue);
+    }
+});
   // TODO: Add code to display the current date in the header of the page.
 });
