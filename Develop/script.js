15be03d887849f10ffc7ab0570428d26895e37cf
nba251522 <inquister112345678@gmail.com>
2023-10-04 21:09:18 -0700

    Added a jquery that utilizes the dayjs format to display the current date on the webpage

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,7 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+      $('#currentDay').text(dayjs().format('MMMM D, YYYY'));
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
